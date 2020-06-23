c3cbb099bb483e08d16efd152162f78cfdb1ba22
KimHeyongSeop <gygy2006@naver.com>
2020-06-23 23:56:27 +0900

    Updates

---
+++ precache-manifest.ed2ece2292842e7af9a1068fa0d2c552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d167e6481907f969c879bd76305e01b",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "84b9be11c627fad852d0",
+    "url": "/movie_app_2019/static/css/main.15290010.chunk.css"
+  },
+  {
+    "revision": "b0c6daa1f2a02f06428e",
+    "url": "/movie_app_2019/static/js/2.031e58d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.031e58d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b9be11c627fad852d0",
+    "url": "/movie_app_2019/static/js/main.6f361da9.chunk.js"
+  },
+  {
+    "revision": "4289ca96aac43a9b8603",
+    "url": "/movie_app_2019/static/js/runtime-main.3e3da9e5.js"
+  }
+]);
